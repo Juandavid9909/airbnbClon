c95618db52e83875fc70cefc6845797ac5f18be3
Juan David <juandavid918@gmail.com>
2023-06-01 20:58:58 -0500

    Ajustado el force-dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import getCurrentUser from "./actions/getCurrentUser";
-import getListings, { IListingsParams } from "./actions/getListings";
+import getListings, { IListingsParams } from "@/app/actions/getListings";
 import ListingCard from "./components/listings/ListingCard";
 
 interface HomeProps {
   searchParams: IListingsParams
 }
+
+export const dynamic = "force-dynamic";
 
 const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
